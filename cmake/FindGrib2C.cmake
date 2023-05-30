87d080b9217de931039eff4aa9e9c5f35c7be04b
Robert Toomey <retoomey@gmail.com>
2023-05-30 23:08:05 +0000

    --minor g2clib cmake tweak for oracle 8 at NSSL.

--- cmake/FindGrib2C.cmake
+++ cmake/FindGrib2C.cmake
@@ -15,7 +15,10 @@ INCLUDE(FindThirdParty)
 find_third_party(
   Grib2C
   HEADER grib2.h
-  LIBRARY grib2c g2c_v1.6.3
+  # g2clib adds versions to the lib name a non standard thing so this will continue to cause
+  # some issues probably.
+  # Oracle 8/bigbang version g2c_v1.6.0
+  LIBRARY grib2c g2c_v1.6.3 g2c_v1.6.0
   HEADER_PATHS ${Grib2C_INCLUDEDIR} ${CUSTOM_THIRDDIR}/include /usr/include
   LIBRARY_PATHS ${Grib2C_LIBRARYDIR} ${CUSTOM_THIRDDIR}/lib /usr/lib64 /usr/lib
 )
