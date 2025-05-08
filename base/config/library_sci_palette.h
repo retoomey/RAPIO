3e0deed0d2360210b0b8c35abd0b37e16218b7eb
gabriela.fisher <gabriela.fisher@gfisher.winstorm.nssl>
2025-05-08 12:15:23 -0500

    added missing header file.

---
+++ base/config/library_sci_palette.h
@@ -0,0 +1,30 @@
+//######################################################################################
+//       library_sci_palette.h
+//       by Brian T Kaney, [2017-2018]
+//######################################################################################
+
+#ifndef SCI_PALETTE_H
+#define SCI_PALETTE_H
+
+#define LOW   0     //--used as the array index to specify the upper
+#define HIGH  1     //--and lower range values in a data palette range 
+
+#define RED   0     //-----to specify color indicies
+#define GREEN 1
+#define BLUE  2
+
+typedef struct { int number_range_colors;
+                 short int **ranges;
+                 int **range_palette;
+
+                 int number_linear_colors;
+                 short int min;
+                 short int max;
+                 int **linear_palette;
+                 } 
+               SCIPalette;  
+
+SCIPalette *SCIPaletteFromFile(char *filename);
+void DestroySCIPalette(SCIPalette *pal);
+
+#endif
