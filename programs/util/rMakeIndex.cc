e67997de3b10f70e5540db9aff2745684cbfea12
Robert Toomey <retoomey@gmail.com>
2026-08-18 23:19:15 -0500

    --fix alias lookup in rMakeIndex.  More work on TimeParser, make general regex and fuzzy for matching file timestamps.  Add one for rap grb2 files as poc. Part of #27

--- programs/util/rMakeIndex.cc
+++ programs/util/rMakeIndex.cc
@@ -9,8 +9,90 @@
 #include "rProcessTimer.h"
 
 #include <fstream>
+#include <regex>
+#include <vector>
+#include <functional>
 
 using namespace rapio;
+
+/** Concrete strategy for Regex-based extraction */
+class RegexTimeParser : public TimeParser {
+private:
+  std::regex myPattern;
+  std::string myFormat;
+  std::function<std::string(const std::smatch&)> myExtractor;
+
+public:
+  RegexTimeParser(std::string name, std::regex p, std::string f, std::function<std::string(const std::smatch&)> ext)
+    : TimeParser(name), myPattern(std::move(p)), myFormat(std::move(f)), myExtractor(std::move(ext)){ }
+
+  bool
+  scan(const std::string& str, Time& set) override
+  {
+    if (str.empty()) { return false; }
+    std::smatch match;
+
+    if (std::regex_search(str, match, myPattern)) {
+      bool success = set.putString(myExtractor(match), myFormat);
+      if (success) {
+        myMatchCount++;
+      }
+      return success;
+    }
+    return false;
+  }
+};
+
+/** Concrete strategy for the original fuzzy digit-counting fallback */
+class FuzzyTimeParser : public TimeParser {
+public:
+
+  FuzzyTimeParser() : TimeParser("Fuzzy"){ }
+
+  bool
+  scan(const std::string& str, Time& set) override
+  {
+    if (str.empty()) { return false; }
+
+    bool success = false;
+    std::string v;
+
+    // Filter to all digits
+    std::copy_if(str.begin(), str.end(), std::back_inserter(v), [](char c) {
+      return std::isdigit(c); // Include only digits
+    });
+
+    // We're expecting a couple of sizes.
+    if (v.size() == 17) { // 19990503232153893
+      success = set.putString(v, "%Y%m%d%H%M%S%/ms");
+    } else if (v.size() == 14) { // 19990503232153
+      success = set.putString(v, "%Y%m%d%H%M%S");
+    }
+    if (success) {
+      myMatchCount++;
+    }
+    return success;
+  }
+};
+
+/** Initialize the dir walker and load the parser cache into the member variable */
+makeIndexDirWalker::makeIndexDirWalker(const std::string& what, MakeIndex * owner, const std::string& suffix)
+  : myWhat(what), myOwner(owner), mySuffix(suffix)
+{
+  // Add Explicit Pattern: rap_130_YYYYMMDD_HHMM_000
+  myParsers.push_back(std::make_shared<RegexTimeParser>("RAP",
+    std::regex(R"(_(\d{8})_(\d{4})_)"),
+    "%Y%m%d%H%M%S",
+    [](const std::smatch& m) {
+    return m[1].str() + m[2].str() + "00";
+  }
+  ));
+
+  // Add future RegexTimeParsers here...
+
+  // Add Fuzzy Fallback as the last resort
+  myParsers.push_back(std::make_shared<FuzzyTimeParser>());
+}
 
 DirWalker::Action
 makeIndexDirWalker::processRegularFile(const std::string& filePath, const struct stat * info)
@@ -25,62 +107,24 @@ makeIndexDirWalker::processRegularFile(const std::string& filePath, const struct
 
   Strings::toLower(factory);
   std::string alias = ConfigIODataType::getIODataTypeFromSuffix(factory);
-  std::shared_ptr<IODataType> f = Factory<IODataType>::get(factory, "IODataType:" + factory);
+  std::shared_ptr<IODataType> f = Factory<IODataType>::get(alias, "IODataType:" + alias);
 
   // Step 2: Get the Time from the filename.
-
-  // -----------------------------------------------------------
-  // Generalize into RAPIO, allow introduction of new parsers if
-  // we're trying to read other formats with more complicated time
-  // strings. For MRMS/HMRG I think the digit approach handles most
-  // cases.
-  //
-  class TimeParser {
-public:
-    TimeParser(){ };
-
-    /** Scan a fuzzy std::string and prepare for time conversion */
-    virtual bool
-    scan(const std::string& str, Time& set)
-    {
-      if (str.empty()) { return false; }
-
-      bool success = false;
-
-      // First, reduce to numbers and ignore everything else
-      // For our standard format, this will either give us
-      // 19990503-232153.893.netcdf.gz  --> 19990503232153893 (17 digits)
-      // 19990503-232153.netcdf.gz --> 19990503232153 (13 digits)
-      // REFL.20220202.222000.gz   --> 20220202222000 (13 digits)
-      //
-      // and this will crazy handle things like:
-      // Somestuff_otherstuff_1990503___232153.893_KTLX_MORE.netcdf.gz
-      // Assuming the numbers are in the 'standard' order we use.
-
-      // Filter to all digits
-      std::string v;
-
-      std::copy_if(str.begin(), str.end(), std::back_inserter(v), [](char c) {
-        return std::isdigit(c); // Include only digits
-      });
-
-      // We're expecting a couple of sizes.
-      if (v.size() == 17) { // 19990503232153893
-        success = set.putString(v, "%Y%m%d%H%M%S%/ms");
-      } else if (v.size() == 14) { // 19990503232153
-        success = set.putString(v, "%Y%m%d%H%M%S");
-      }
-      return success;
-    }
-  };
-
   if (f != nullptr) {
     std::string localFile = filePath.substr(getFileOffset());
     Time aTime;
 
-    TimeParser test;
-    if (test.scan(localFile, aTime)) {
-      fLogDebug("FILE:{} has BUILDER:({})", filePath, factory);
+    // Iterate over the member variable parsers
+    bool parsedSuccessfully = false;
+    for (const auto& parser : myParsers) {
+      if (parser->scan(localFile, aTime)) {
+        parsedSuccessfully = true;
+        break;
+      }
+    }
+
+    if (parsedSuccessfully) {
+      fLogDebug("FILE:{} has BUILDER:({})", filePath, alias);
       // printPath("File: ", filePath, info);
 
       // Remove begining and end of the path
@@ -94,18 +138,13 @@ public:
       Strings::splitWithoutEnds(subtree, '/', &fields);
 
       // The 'size' of subtree will match the depth now.
-
       // -------------------------------------------------------------------
-      //
       // OK, make a record.  Since we're not an algorithm the queue shouldn't
       // empty, but it will still sort. So we buffer the entire archive.
-      // FIXME: This is very similar to rFileIndex.cc.  We should probably
-      // make common code doing this, maybe in ConfigRecord
       std::vector<std::string> params;
-      std::vector<std::string> selects;
 
       // First param is the builder
-      params.push_back(factory);
+      params.push_back(alias);
 
       const bool fullMode = false; // do builder filename only
       if (fullMode) {
@@ -113,35 +152,36 @@ public:
       } else {
         // Do the indexlocation and spaced out mode.
         params.push_back(Constants::IndexPathReplace);
-        for (size_t i = 0; i < fields.size(); ++i) {
-          params.push_back(fields[i]); // MRMS/RAPIO just append together
+        for (const auto& field : fields) {
+          params.push_back(field); // MRMS/RAPIO just append together
         }
         params.push_back(localFile);
       }
 
-      // The display handles three fields, so the second field we'll
-      // compress the fields, giving us say:
-      // KTLX, Reflectivity, 00.50 --> KTLX_Reflectivity 00.50
-      size_t at = 0;
-      selects.push_back(""); // time field
-      if (fields.size() > 2) {
-        at = fields.size() - 1; // skip later
+      // Extract dataType and subType from the directory fields
+      // Example: /KTLX/Reflectivity/00.50/ -> dataType="KTLX_Reflectivity", subType="00.50"
+      std::string dataType = "unknown";
+      std::string subType  = "default";
 
-        // Compress first n-2 items into one selection
-        std::string out;
-        for (size_t i = 0; i < at; ++i) {
-          out += fields[i];
-          if (i != at - 1) { out += "_"; }
+      if (!fields.empty()) {
+        if (fields.size() == 1) {
+          // Only one directory deep
+          dataType = fields[0];
+        } else {
+          // Two or more directories deep
+          subType = fields.back(); // Last directory is the subtype
+
+          // Squash everything before the last directory into the dataType
+          dataType = fields[0];
+          for (size_t i = 1; i < fields.size() - 1; ++i) {
+            dataType += "_" + fields[i];
+          }
         }
-        selects.push_back(out);
       }
-      for (size_t i = at; i < fields.size(); ++i) {
-        selects.push_back(fields[i]);
-      }
-      if (selects.size() > 1) {
-        Record rec(params, factory, aTime, selects[1], selects[2]);
-        Record::theRecordQueue->addRecord(rec);
-      }
+
+      // Create and enqueue the record
+      Record rec(params, factory, aTime, dataType, subType);
+      Record::theRecordQueue->addRecord(rec);
       // -------------------------------------------------------------------
     } else {
       // Couldn't time scan, so what next?
@@ -160,6 +200,16 @@ makeIndexDirWalker::processDirectory(const std::string& dirPath, const struct st
   fLogDebug("DIR: {} {}", dirPath, getDepth());
   // printPath("Dir: ", dirPath, info);
   return DirWalker::Action::CONTINUE; // Process the root
+}
+
+void
+makeIndexDirWalker::printStats()
+{
+  // Iterate over the member variable parsers
+  for (const auto& parser : myParsers) {
+    // Could let the parser print
+    fLogInfo("TimeParser '{}' matched {} files", parser->getName(), parser->getMatchCount());
+  }
 }
 
 void
@@ -206,6 +256,7 @@ MakeIndex::execute()
   walk.traverse(myRoot);
   fLogInfo("Processed {} total files.", walk.getFileCounter());
   fLogInfo("Gathered a total of {} records", myQueue.size());
+  walk.printStats();
 
   // --------------------------------------------------------
   // Write xml output file
--- programs/util/rMakeIndex.h
+++ programs/util/rMakeIndex.h
@@ -2,8 +2,43 @@
 
 #include <rRAPIOProgram.h>
 #include <rDirWalker.h>
+#include <memory>
+#include <vector>
 
 namespace rapio {
+// -----------------------------------------------------------
+// FIXME: Generalize into RAPIO, allow introduction of new parsers if
+// we're trying to read other formats with more complicated time
+// strings. For MRMS/HMRG I think the digit approach handles most
+// cases. But it didn't handle the rap data format.  And I'm sure
+// there are tons of others since nothing is standardized.
+// I think some of Time.h could be moved into a TimeParser class.
+//
+/** Base interface for all time extraction strategies */
+class TimeParser {
+public:
+  TimeParser(const std::string& name) : myName(name){ }
+
+  virtual
+  ~TimeParser() = default;
+
+  /** Name of this time parser */
+  const std::string&
+  getName() const { return myName; }
+
+  /** How many matched strings? */
+  size_t
+  getMatchCount() const { return myMatchCount; }
+
+
+  /** Scan a filename string and attempt to populate the Time object. */
+  virtual bool
+  scan(const std::string& str, Time& set) = 0;
+protected:
+  size_t myMatchCount = 0;
+  std::string myName;
+};
+
 /*
  * Yet another make index.
  *
@@ -45,9 +80,7 @@ class makeIndexDirWalker : public DirWalker
 {
 public:
   /** Create dir walker helper */
-  makeIndexDirWalker(const std::string& what, MakeIndex * owner, const std::string& suffix) : myWhat(what), myOwner(
-      owner), mySuffix(suffix)
-  { }
+  makeIndexDirWalker(const std::string& what, MakeIndex * owner, const std::string& suffix);
 
   /** Process a regular file */
   virtual Action
@@ -56,6 +89,10 @@ public:
   /** Process a directory */
   virtual Action
   processDirectory(const std::string& dirPath, const struct stat * info) override;
+
+  /** Print final stats */
+  void
+  printStats();
 
 protected:
 
@@ -67,5 +104,8 @@ protected:
 
   /** The suffix we are hunting such as ".cache" or ".mask" */
   std::string mySuffix;
+
+  /** Our chain of cached time parsers */
+  std::vector<std::shared_ptr<TimeParser> > myParsers;
 };
 }
